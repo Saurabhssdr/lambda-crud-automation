57afc983dc2740fa3f5700293e216c1f29361225
Saurabhssdr <124699813+Saurabhssdr@users.noreply.github.com>
2025-07-05 18:57:14 +0530

    Update setup.sh

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -10,13 +10,24 @@ cd /home/ec2-user
 # Clone repo
 git clone https://github.com/Saurabhssdr/fast-api.git
 cd fast-api
-git checkout main
- 
-# # Ensure .env is present
-# if [ ! -f ".env" ]; then
-#     echo "❌ .env file missing in the repo. Exiting."
-#     exit 1
-# fi
+
+# Create .env file with placeholders
+cat << 'EOF' > /home/ec2-user/fast-api/.env
+AWS_ACCESS_KEY_ID= #AWS_ACCESS_KEY_ID#
+AWS_SECRET_ACCESS_KEY= #AWS_SECRET_ACCESS_KEY#
+AWS_DEFAULT_REGION= #AWS_DEFAULT_REGION#
+EOF
+
+# Ensure .env is created
+if [ ! -f ".env" ]; then
+    echo "❌ .env file creation failed. Exiting."
+    exit 1
+fi
+
+sed -i 's/#AWS_ACCESS_KEY_ID#/AKIA2L32X3A2J362ZXSB/g' /home/ec2-user/fast-api/.env
+sed -i 's/#AWS_SECRET_ACCESS_KEY#/SID6XPTVWGmWbwUMzZXZGamZ8Dro4kY1+194fAN/g' /home/ec2-user/fast-api/.env
+sed -i 's/#AWS_DEFAULT_REGION#/us-east-1/g' /home/ec2-user/fast-api/.env
+
  
 # Install Python dependencies
 pip3 install --upgrade pip
