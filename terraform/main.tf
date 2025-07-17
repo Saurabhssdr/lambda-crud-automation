e86939185bdf9452aae411baacab167119e0553f
Saurabhssdr <124699813+Saurabhssdr@users.noreply.github.com>
2025-07-17 15:00:43 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
  
 # 1. IAM Role for EC2 to access DynamoDB
-resource "aws_iam_role" "ec2_dynamo_role-v3" {
-  name = "ec2-dynamo-role-v3"
+resource "aws_iam_role" "ec2_dynamo_role-v4" {
+  name = "ec2-dynamo-role-v4"
  
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -20,14 +20,14 @@ resource "aws_iam_role" "ec2_dynamo_role-v3" {
  
 # 2. Attach AmazonDynamoDBFullAccess policy to that role
 resource "aws_iam_role_policy_attachment" "dynamodb_access" {
-  role       = aws_iam_role.ec2_dynamo_role-v3.name
+  role       = aws_iam_role.ec2_dynamo_role-v4.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
  
 # 3. Create an instance profile from that role
-resource "aws_iam_instance_profile" "ec2_profile-v3" {
-  name = "ec2-instance-profile-v3"
-  role = aws_iam_role.ec2_dynamo_role-v3.name
+resource "aws_iam_instance_profile" "ec2_profile-v4" {
+  name = "ec2-instance-profile-v4"
+  role = aws_iam_role.ec2_dynamo_role-v4.name
 }
  
 # 4. Create EC2 instance and attach role
@@ -35,7 +35,7 @@ resource "aws_instance" "fastapi_ec2" {
   ami                    = "ami-051f8a213df8bc089"  # Amazon Linux 2 (check latest for your region)
   instance_type          = "t2.micro"
   key_name               = "my-key-pem"             # Replace with your key
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile-v3.name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile-v4.name
   user_data              = file("${path.module}/setup.sh")
  
   tags = {
