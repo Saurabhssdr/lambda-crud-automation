816d12bf182551f29f01fad31a02b088c9113767
Saurabhssdr <124699813+Saurabhssdr@users.noreply.github.com>
2025-07-17 16:18:24 +0530

    Update setup.sh

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 yum update -y
-yum install -y git python3-pip
+yum install -y git docker
+systemctl start docker
+systemctl enable docker
+
 cd /home/ec2-user
 git clone https://github.com/Saurabhssdr/fast-api.git
 cd fast-api
+
 chown ec2-user:ec2-user /home/ec2-user/fast-api
 chmod 755 /home/ec2-user/fast-api
-pip3 install --upgrade pip
-pip3 install -r requirements.txt || pip3 install fastapi uvicorn boto3 python-dotenv httpx
-nohup uvicorn main:app --host 0.0.0.0 --port 8000 > fastapi.log 2>&1 &
+
+docker build -t fastapi-crud .
+docker run -d -p 8000:80 fastapi-crud
 
