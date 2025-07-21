c502286e73c6ccb2c5de183325ab9aa5a75b7f74
Saurabhssdr <124699813+Saurabhssdr@users.noreply.github.com>
2025-07-21 12:21:51 +0530

    Update setup.sh

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,31 +1,3 @@
-# #!/bin/bash
-
-# # Update and install dependencies
-# yum update -y
-# yum install -y git docker
-# systemctl start docker
-# systemctl enable docker
-
-# # Add ec2-user to docker group (no reboot needed if service is running)
-# usermod -aG docker ec2-user
-# newgrp docker  # Apply group change immediately
-
-# # Clone repo
-# cd /home/ec2-user
-# git clone https://github.com/Saurabhssdr/fast-api.git || (cd fast-api && git pull)
-# cd fast-api
-
-# # Set permissions
-# chown -R ec2-user:ec2-user /home/ec2-user/fast-api
-# chmod -R 755 /home/ec2-user/fast-api
-
-# # Rename dockerfile if needed
-# [ -f dockerfile ] && mv dockerfile Dockerfile
-
-# # Build and run Docker container
-# docker build -t fastapi-crud .
-# docker run -d -p 8000:80 --restart unless-stopped --name fastapi-crud fastapi-crud
-
 #!/bin/bash
 yum update -y
 yum install -y git python3-pip
