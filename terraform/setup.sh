75f22f82f50afc1f1fdec43ccd587d10d0e60718
Saurabhssdr <124699813+Saurabhssdr@users.noreply.github.com>
2025-07-30 18:56:55 +0530

    Update setup.sh

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,3 +1,31 @@
+# #!/bin/bash
+
+# # Update and install dependencies
+# yum update -y
+# yum install -y git docker
+# systemctl start docker
+# systemctl enable docker
+
+# # Add ec2-user to docker group (no reboot needed if service is running)
+# usermod -aG docker ec2-user
+# newgrp docker  # Apply group change immediately
+
+# # Clone repo
+# cd /home/ec2-user
+# git clone https://github.com/Saurabhssdr/fast-api.git || (cd fast-api && git pull)
+# cd fast-api
+
+# # Set permissions
+# chown -R ec2-user:ec2-user /home/ec2-user/fast-api
+# chmod -R 755 /home/ec2-user/fast-api
+
+# # Rename dockerfile if needed
+# [ -f dockerfile ] && mv dockerfile Dockerfile
+
+# # Build and run Docker container
+# docker build -t fastapi-crud .
+# docker run -d -p 8000:80 --restart unless-stopped --name fastapi-crud fastapi-crud
+
 #!/bin/bash
 
 # Update and install dependencies
@@ -12,12 +40,12 @@ newgrp docker  # Apply group change immediately
 
 # Clone repo
 cd /home/ec2-user
-git clone https://github.com/Saurabhssdr/fast-api.git || (cd fast-api && git pull)
-cd fast-api
+git clone https://github.com/Saurabhssdr/lambda-crud-automation.git || (cd lambda-crud-automation && git pull)
+cd lambda-crud-automation
 
 # Set permissions
-chown -R ec2-user:ec2-user /home/ec2-user/fast-api
-chmod -R 755 /home/ec2-user/fast-api
+chown -R ec2-user:ec2-user /home/ec2-user/lambda-crud-automation
+chmod -R 755 /home/ec2-user/lambda-crud-automation
 
 # Rename dockerfile if needed
 [ -f dockerfile ] && mv dockerfile Dockerfile
@@ -25,4 +53,3 @@ chmod -R 755 /home/ec2-user/fast-api
 # Build and run Docker container
 docker build -t fastapi-crud .
 docker run -d -p 8000:80 --restart unless-stopped --name fastapi-crud fastapi-crud
-
